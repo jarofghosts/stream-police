7c3f4e17a84dd42614e9067e344b79c9cc006a62
jesse keane <me@jessekeane.me>
2013-10-08 21:12:54 -0700

    iunoo

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-var through = require('through');
+var through = require('through')
 
-module.exports = police;
+module.exports = police
 
 function police(options) {
-  options = options || {};
+  options = options || {}
   var exclude = false,
-      verify = false;
+      verify = false
+
   if (options.exclude) {
-    exclude = true;
-    var excludeLength = options.exclude.length;
+    exclude = true
+    var excludeLength = options.exclude.length
   }
   if (options.verify) {
-    verify = true;
-    var verifyLength = options.verify.length;
+    verify = true
+    var verifyLength = options.verify.length
   }
-  var tr = through(write);
+  var tr = through(write)
+  return tr
 
   function write(buf) {
     var str = buf.toString();
     if (exclude) {
-      var i = 0;
+      var i = 0
       for (; i < excludeLength; ++i) {
-        if (options.exclude[i].test(str)) return;
+        if (options.exclude[i].test(str)) return
       }
     }
     if (verify) {
-      var i = 0;
+      var i = 0
       for (; i < verifyLength; ++i) {
-        if (options.verify[i].test(str)) {
-          this.queue(buf);
-          return;
-        }
+        if (options.verify[i].test(str))  return this.queue(buf)
       }
-      return;
+      return
     }
-    this.queue(buf);
+    this.queue(buf)
   }
-  return tr;
 }
 
